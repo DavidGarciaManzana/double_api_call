164a55cc23904d87640ece0100e159af02059ab6
David Garcia <davidgarciama@outlook.com>
2023-04-17 14:53:50 -0600

    Fix GA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   const variableGAn =`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag("js", new Date());
+  gtag(\"js\", new Date());
 
-  gtag("config", "${process.env.NEXT_PUBLIC_GA_KEY}");`
+  gtag(\"config\", \"${process.env.NEXT_PUBLIC_GA_KEY}\");`
   
   return (
     <Html lang="en">
