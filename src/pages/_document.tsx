a792759ff953412f348965d78a540a7ba7347c71
David Garcia <davidgarciama@outlook.com>
2023-04-17 15:09:43 -0600

    Fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,20 +9,6 @@ export default function Document() {
   
     <Html lang="en">
       <Head />
-
-
-      <Script id={'GA1'} strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_KEY}`} />
-
-      <Script id={'GA2'} strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_KEY}', {
-          page_path: window.location.pathname,
-          });
-        `}
-      </Script>
       <body>
         <Main/>
         <NextScript />
