6dea87ab7c56a1644633402f0c5c9bfa7aacddc2
David Garcia <davidgarciama@outlook.com>
2023-04-17 14:50:14 -0600

    Fix GA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   const variableGAn =`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  gtag("js", new Date());
 
-  gtag('config', '${process.env.NEXT_PUBLIC_GA_KEY}');`
+  gtag("config", "${process.env.NEXT_PUBLIC_GA_KEY}");`
   
   return (
     <Html lang="en">
       <Head />
 
-<script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_KEY}`}></script>
-<script>
-{variableGAn}
-</script>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_KEY}`}></script>
+      <script>
+        {variableGAn}
+      </script>
       <body>
         <Main/>
         <NextScript />
