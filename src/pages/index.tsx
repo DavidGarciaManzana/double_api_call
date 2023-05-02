8b8b80d50403b0f46a9b5bc18308081f54365ea0
David Garcia <davidgarciama@outlook.com>
2023-05-02 15:19:15 -0600

    Google analytics added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,43 @@ import APIProvider from "@/pages/APIProvider/APIProvider";
 import StatusProvider from "@/pages/StatusProvider/StatusProvider";
 import App from "@/pages/App/App"
 import {GetServerSideProps} from "next";
+import Script from "next/script";
 
 interface HomeProps {
-    ip:string
+    ip: string
 }
 
-export default function Home({ip}:HomeProps) {
+export default function Home({ip}: HomeProps) {
     return (
-        <StatusProvider>
-            <IngredientsProvider>
-                <APIProvider ip={ip}>
-                    <App ip={ip}/>
-                </APIProvider>
-            </IngredientsProvider>
-        </StatusProvider>
+        <>
+            <StatusProvider>
+                <IngredientsProvider>
+                    <APIProvider ip={ip}>
+                        <App ip={ip}/>
+                    </APIProvider>
+                </IngredientsProvider>
+            </StatusProvider>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_KEY}`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_KEY}');
+        `}
+            </Script>
+        </>
     )
 }
 
-export const  getServerSideProps: GetServerSideProps<Object>= async (context) => {
+export const getServerSideProps: GetServerSideProps<Object> = async (context) => {
     let ip;
 
-    const { req } = context;
+    const {req} = context;
 
     if (req.headers['x-forwarded-for']) {
         // @ts-ignore
